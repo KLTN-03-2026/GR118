250ab05fad14192f08cb16f6cba3d20d0b6c7a85
VietD15 <ndviet1501@gmail.com>
2026-05-21 00:42:41 +0700

    fix OTP

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,8 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import app from "./app";
 import { connectDB } from "./config/database";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const PORT = process.env.PORT ;
 
